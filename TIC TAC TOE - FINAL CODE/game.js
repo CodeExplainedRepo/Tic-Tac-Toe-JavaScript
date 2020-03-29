233f10511a50ea4ea567a3c1d3cdd3a11f809c2e
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2020-03-30 00:00:22 +0100

    Update game.js

--- TIC TAC TOE - FINAL CODE/game.js
+++ TIC TAC TOE - FINAL CODE/game.js
@@ -1,36 +1,28 @@
 function init(player, OPPONENT){
-    // SELECT THE CANVAS
+    // SELECT CANAVS
     const canvas = document.getElementById("cvs");
     const ctx = canvas.getContext("2d");
 
-    // load images
+    // BOARD VARIABLES
+    let board = [];
+    const COLUMN = 3;
+    const ROW = 3;
+    const SPACE_SIZE = 150;
+
+    // STORE PLAYER'S MOVES
+    let gameData = new Array(9);
+    
+    // By default the first player to play is the human
+    let currentPlayer = player.man;
+
+    // load X & O images
     const xImage = new Image();
     xImage.src = "img/X.png";
 
     const oImage = new Image();
     oImage.src = "img/O.png";
 
-    // GIVE THE FIRST TURN TO HUMAN
-    let currentPlayer = player.man;
-
-    // Where we save the moves
-    let gameData =  new Array(9);
-
-    // every tile has an id, example: board[1][1] = 5
-    // so when we click on a tile we will know teh exact id of the clicked tile.
-    let board = [];
-
-    // The board is 3x3
-    const column = 3;
-    const row = 3;
-
-    // wait for the computer to play
-    let WAIT = false;
-
-    // game over
-    let GAME_OVER = false;
-
-    // we win the game when we have one of these combinations
+    // Win combinations
     const COMBOS = [
         [0, 1, 2],
         [3, 4, 5],
@@ -42,264 +34,236 @@ function init(player, OPPONENT){
         [2, 4, 6]
     ];
 
-    // A tile is 150x150px
-    const SPACE_SIZE = 150;
-
+    // FOR GAME OVER CHECK
+    let GAME_OVER = false;
+    
     // DRAW THE BOARD
     function drawBoard(){
-        // first tile has the id=0
-        let id = 0;
-        
-        // draw all the tiles
-        for(let i=0; i < row; i++){
+        // WE give every space a unique id
+        // So we know exactly where to put the player's move on the gameData Array
+        let id = 0
+        for(let i = 0; i < ROW; i++){
             board[i] = [];
-            for(let j=0; j < column; j++){
-                // give an id from 0 to 8 to the board tiles.
+            for(let j = 0; j < COLUMN; j++){
                 board[i][j] = id;
-                
-                let X = j;
-                let Y = i;
-                // draw tiles
+                id++;
+
+                // draw the spaces
                 ctx.strokeStyle = "#000";
                 ctx.strokeRect(j * SPACE_SIZE, i * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
-
-                // increment the id each time we go to the next tile
-                id++
             }
         }
     }
     drawBoard();
 
+    // ON PLAYER'S CLICK
     canvas.addEventListener("click", function(event){
-        // if game over, exit
+        
+        // IF IT's A GAME OVER? EXIT
         if(GAME_OVER) return;
 
-        // if the computer didn't play yet; wait
-        if(WAIT) return;
-
-        console.log(event);
-
-        // X & Y position of mouse click
+        // X & Y position of mouse click relative to the canvas
         let X = event.clientX - canvas.getBoundingClientRect().x;
         let Y = event.clientY - canvas.getBoundingClientRect().y;
 
-        // i & j of the cicked tile
+        // WE CALCULATE i & j of the clicked SPACE
         let i = Math.floor(Y/SPACE_SIZE);
         let j = Math.floor(X/SPACE_SIZE);
 
-        // update the gameData
-        if(gameData[getIdOfTile(i, j)]) return;
-        gameData[getIdOfTile(i, j)] = currentPlayer;
+        // Get the id of the space the player clicked on
+        let id = board[i][j];
+
+        // Prevent the player to play the same space twice
+        if(gameData[id]) return;
+
+        // store the player's move to gameData
+        gameData[id] = currentPlayer;
         
-        // draw on board
+        // draw the move on board
         drawOnBoard(currentPlayer, i, j);
 
-        // check for winner or tie
+        // Check if the play wins
         if(isWinner(gameData, currentPlayer)){
             showGameOver(currentPlayer);
             GAME_OVER = true;
             return;
         }
 
-        // Check for draw
+        // check if it's a tie game
         if(isTie(gameData)){
             showGameOver("tie");
             GAME_OVER = true;
             return;
         }
 
-        // RUN AI
-        if(OPPONENT == "computer"){
+        if( OPPONENT == "computer"){
+            // get id of space using minimax algorithm
+            let id = minimax( gameData, player.computer ).id;
 
-            // if game over, exit
-            if(GAME_OVER) return;
-        
-            WAIT = true;
-
-            let id = AIMove();
-
-            // draw on board
-            let IJ = getIJOfTile(id);
-            drawOnBoard(player.computer, IJ.i, IJ.j);
-
-            // UPDATE GAME DATA
+            // store the player's move to gameData
             gameData[id] = player.computer;
+            
+            // get i and j of space
+            let space = getIJ(id);
 
-            // check for winner or tie
+            // draw the move on board
+            drawOnBoard(player.computer, space.i, space.j);
+
+            // Check if the play wins
             if(isWinner(gameData, player.computer)){
                 showGameOver(player.computer);
                 GAME_OVER = true;
                 return;
             }
 
-            // check for draw
+            // check if it's a tie game
             if(isTie(gameData)){
                 showGameOver("tie");
                 GAME_OVER = true;
                 return;
             }
-
-            WAIT = false;
         }else{
-            // if the openent is a friend
-            // change turn
+            // GIVE TURN TO THE OTHER PLAYER
             currentPlayer = currentPlayer == player.man ? player.friend : player.man;
-        
-            
         }
+
     });
 
-    function AIMove(){
-        // Check for empty tiles
-        let emptyTiles = [];
-        for(let i = 0; i < gameData.length; i++){
-            if(!gameData[i]){
-                emptyTiles.push(i);
+    // MINIMAX
+    function minimax(gameData, PLAYER){
+        // BASE
+        if( isWinner(gameData, player.computer) ) return { evaluation : +10 };
+        if( isWinner(gameData, player.man)      ) return { evaluation : -10 };
+        if( isTie(gameData)                     ) return { evaluation : 0 };
+
+        // LOOK FOR EMTY SPACES
+        let EMPTY_SPACES = getEmptySpaces(gameData);
+
+        // SAVE ALL MOVES AND THEIR EVALUATIONS
+        let moves = [];
+
+        // LOOP OVER THE EMPTY SPACES TO EVALUATE THEM
+        for( let i = 0; i < EMPTY_SPACES.length; i++){
+            // GET THE ID OF THE EMPTY SPACE
+            let id = EMPTY_SPACES[i];
+
+            // BACK UP THE SPACE
+            let backup = gameData[id];
+
+            // MAKE THE MOVE FOR THE PLAYER
+            gameData[id] = PLAYER;
+
+            // SAVE THE MOVE'S ID AND EVALUATION
+            let move = {};
+            move.id = id;
+            // THE MOVE EVALUATION
+            if( PLAYER == player.computer){
+                move.evaluation = minimax(gameData, player.man).evaluation;
+            }else{
+                move.evaluation = minimax(gameData, player.computer).evaluation;
             }
+
+            // RESTORE SPACE
+            gameData[id] = backup;
+
+            // SAVE MOVE TO MOVES ARRAY
+            moves.push(move);
         }
 
-        // Check for empty corner tiles
-        let emptyCornerTiles = [];
-        const corners = [0, 2, 6, 8];
-        for(let i = 0; i < emptyTiles.length; i++){
-            for( let j = 0; j < corners.length; j++){
-                if( corners[j] == emptyTiles[i]){
-                    emptyCornerTiles.push(corners[j]);
+        // MINIMAX ALGORITHM
+        let bestMove;
+
+        if(PLAYER == player.computer){
+            // MAXIMIZER
+            let bestEvaluation = -Infinity;
+            for(let i = 0; i < moves.length; i++){
+                if( moves[i].evaluation > bestEvaluation ){
+                    bestEvaluation = moves[i].evaluation;
+                    bestMove = moves[i];
+                }
+            }
+        }else{
+            // MINIMIZER
+            let bestEvaluation = +Infinity;
+            for(let i = 0; i < moves.length; i++){
+                if( moves[i].evaluation < bestEvaluation ){
+                    bestEvaluation = moves[i].evaluation;
+                    bestMove = moves[i];
                 }
             }
         }
 
-        // check for empty edge tiles
-        let emptyEdgeTiles = [];
-        const edges = [1, 3, 5, 7];
-        for(let i = 0; i < emptyTiles.length; i++){
-            for( let j = 0; j < edges.length; j++){
-                if( edges[j] == emptyTiles[i]){
-                    emptyEdgeTiles.push(edges[j]);
-                }
-            }
-        }
-
-        // check For Best Move
-        
-        // FIRST FOR COMPUTER
-        for(let i = 0; i < emptyTiles.length; i++){
-            let gameDataCopy = [...gameData];
-            gameDataCopy[emptyTiles[i]] = player.computer;
-            
-            if(isWinner(gameDataCopy, player.computer)){
-                return emptyTiles[i];
-            }
-        }
-        // THEN FOR MAN
-        for(let i = 0; i < emptyTiles.length; i++){
-            let gameDataCopy = [...gameData];
-            gameDataCopy[emptyTiles[i]] = player.man;
-            
-            if(isWinner(gameDataCopy, player.man)){
-                return emptyTiles[i];
-            }
-        }
-
-        // center tile id
-        let centerTileId = 4;
-
-        // select center tile if it's empty
-        for(let i = 0; i < emptyTiles.length; i++){
-            if(emptyTiles[i] == centerTileId){
-                console.log("empty center");
-                return centerTileId;
-            }
-        }
-
-        // if the user toke the center, then go for a corner tile
-        
-        // if the user didn't take the center, then go for an edge tile.
-
-        // SELECT a CORNER RANDOMLY
-        if(emptyCornerTiles.length > 0){
-            let randomIndex = Math.floor(Math.random() * emptyCornerTiles.length);
-            id = emptyCornerTiles[randomIndex];
-            return id;
-        }   
-
-        // SELECT an EDGE RANDOMLY
-        if(emptyEdgeTiles.length > 0){
-            let randomIndex = Math.floor(Math.random() * emptyEdgeTiles.length);
-            id = emptyEdgeTiles[randomIndex];
-            return id;
-        }
-
-
+        return bestMove;
     }
 
-    function getIdOfTile(i, j){
-        return board[i][j]; 
+    // GET EMPTY SPACES
+    function getEmptySpaces(gameData){
+        let EMPTY = [];
+
+        for( let id = 0; id < gameData.length; id++){
+            if(!gameData[id]) EMPTY.push(id);
+        }
+
+        return EMPTY;
     }
 
-    function getIJOfTile(id){
-        for(let i=0; i < row; i++){
-            for(let j=0; j < column; j++){
-                if(board[i][j] == id){
-                    let IJ = {
-                        i : i,
-                        j : j
-                    }
-                    return IJ;
-                }
+    // GET i AND j of a SPACE
+    function getIJ(id){
+        for(let i = 0; i < board.length; i++){
+            for(let j = 0; j < board[i].length; j++){
+                if(board[i][j] == id) return { i : i, j : j}
             }
         }
     }
 
-    function drawOnBoard(player, i, j){
-        const img = player == "X" ? xImage : oImage;
-        ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE);
-    }
-
-    function isWinner(data, player){
-
+    // check for a winner
+    function isWinner(gameData, player){
         for(let i = 0; i < COMBOS.length; i++){
             let won = true;
 
             for(let j = 0; j < COMBOS[i].length; j++){
                 let id = COMBOS[i][j];
-                won = data[id] == player && won;
+                won = gameData[id] == player && won;
             }
 
             if(won){
                 return true;
             }
         }
-
         return false;
     }
 
+    // Check for a tie game
     function isTie(gameData){
         let isBoardFill = true;
-        
         for(let i = 0; i < gameData.length; i++){
             isBoardFill = gameData[i] && isBoardFill;
         }
-
         if(isBoardFill){
             return true;
         }
-
         return false;
     }
 
+    // SHOW GAME OVER
     function showGameOver(player){
+        let message = player == "tie" ? "Oops No Winner" : "The Winner is";
         let imgSrc = `img/${player}.png`;
-        let message = player == "tie" ? "Oops No Winner" : "The winner is";
-
-        
 
         gameOverElement.innerHTML = `
-            <h1>${message}</h1>
-            <img class="winner-img" src=${imgSrc} alt="">
-            <div class="play" onclick="location.reload();">PLAY AGAIN!</div>
-            `;
-        
-            gameOverElement.classList.remove("hide");
+            <h1>${message}</1>
+            <img class="winner-img" src=${imgSrc} </img>
+            <div class="play" onclick="location.reload()">Play Again!</div>
+        `;
+
+        gameOverElement.classList.remove("hide");
+    }
+
+    // draw on board
+    function drawOnBoard(player, i, j){
+        let img = player == "X" ? xImage : oImage;
+
+        // the x,y positon of the image are the x,y of the clicked space
+        ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE);
     }
 }
